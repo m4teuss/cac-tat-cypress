70a6483e1a6e6326ca5a9797d0e8ad315d1be5ec
m4teuss <mateuus.s.silva@gmail.com>
2022-03-11 22:26:17 -0300

    typing on input and click on elements

--- cypress/integration/cac-tat.spec.js
+++ cypress/integration/cac-tat.spec.js
@@ -15,4 +15,15 @@ describe('Central de atendimento do cliente TAT', function() {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
     });
 
+
+    it.only('preenche os campos obrigatórios e envia o formularo', () =>{
+        cy.get('#firstName').type('mateus')
+        cy.get('#lastName').type('Silva')
+        cy.get('#email').type('mateus@gmail.com')
+        cy.get('#phone').type('11993654471')
+        cy.get('#open-text-area').type('Teste')
+        cy.get('button[type="submit"]').click()
+        cy.get('.success').should('be.visible')
+    })
+
 }); // close describe
